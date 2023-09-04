edbdd2931199cb0bcbb1104e337424fe8745728e
Vella <gksdnjs2@gmail.com>
2023-09-04 17:22:15 +0900

    path 수정

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import '../styles/css/variable.css';
 
 export const metadata = {
